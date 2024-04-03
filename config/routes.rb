beed154816a9bbaf2556292c7900265ca8061702
toshionish <155517110+toshionish@users.noreply.github.com>
2024-04-03 15:23:32 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-#  get 'messages/index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "rooms#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end 
 end
